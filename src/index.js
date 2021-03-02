ba233b88fa24acc27e03c6efb2877cb1f1153675
unsky559 <6kostya@gmail.com>
2021-03-02 17:06:34 +0200

    init: make min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }
+  let minimal = array[0];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] < minimal)
+      minimal = array[i]
+  }
+  return minimal;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }
+  let maximum = array[0];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] > maximum)
+      maximum = array[i]
+  }
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }
+  let result = 0;
+  for(let i = 0; i < array.length; i++){
+    result += array[i]
+  }
+  return result/array.length;
 }
+console.log(exports.avg([12,4,5,8]))
